d44ac84197fa315d285a8525aa398bef007df451
Ragul Kumar A <aragulkumar11@gmail.com>
2026-01-07 06:55:25 +0000

    COMPLETED : setup

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numOfDrumButtons; i++)  {
+
+    document.querySelectorAll("button")[i].addEventListener("click",function () {
+alert("Im working buddy")});
+
+}
